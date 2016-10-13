3749a365826941acf27d650bfada154904875062
subakumaran3 <subashini@cbasolutions.lk>
2016-10-13 12:59:24 +0530

    stage 3

--- src/TestPac/TestMessage.java
+++ src/TestPac/TestMessage.java
@@ -5,6 +5,7 @@ public class TestMessage
 	public static void main(String[] args) 
 	{
 	System.out.println("Hello Test message");
+	System.out.println("Hello Test message 2.0");
 	System.out.println("Hello Test message2.1");
 	}
 }
